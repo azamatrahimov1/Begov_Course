b24ab15b02887fdf5540ea36400379d3b7b9cc3f
Azamat <azamatrahimov80@gmail.com>
2024-05-23 09:52:22 +0500

    online and offline edit.blade.php added some things

--- resources/views/admin/offline/edit.blade.php
+++ resources/views/admin/offline/edit.blade.php
@@ -26,6 +26,27 @@
                     <input type="number" class="form-control" name="price" value="{{ old('price', $offline->price) }}">
                 </div>
                 <div class="mb-3">
+                    <label for="teacher" class="form-label">O'qituvchi</label>
+                    <input type="text" class="form-control" name="teacher" value="{{ old('teacher', $online->teacher) }}">
+                    @error('teacher')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="student" class="form-label">O'quvchi soni</label>
+                    <input type="text" class="form-control" name="student" value="{{ old('student', $online->student) }}">
+                    @error('student')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="hour" class="form-label">Dars Vaqti</label>
+                    <input type="text" class="form-control" name="hour" value="{{ old('hour', $online->hour) }}">
+                    @error('hour')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
                     <label for="desc" class="form-label">Tavsif</label>
                     <textarea name="desc" id="tinymce" class="form-control" rows="5">{{ old('desc', $offline->desc) }}</textarea>
                 </div>
--- resources/views/admin/online/edit.blade.php
+++ resources/views/admin/online/edit.blade.php
@@ -24,6 +24,30 @@
                 <div class="mb-3">
                     <label for="price" class="form-label">Narx</label>
                     <input type="number" class="form-control" name="price" value="{{ old('price', $online->price) }}">
+                    @error('price')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="teacher" class="form-label">O'qituvchi</label>
+                    <input type="text" class="form-control" name="teacher" value="{{ old('teacher', $online->teacher) }}">
+                    @error('teacher')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="student" class="form-label">O'quvchi soni</label>
+                    <input type="text" class="form-control" name="student" value="{{ old('student', $online->student) }}">
+                    @error('student')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="hour" class="form-label">Dars Vaqti</label>
+                    <input type="text" class="form-control" name="hour" value="{{ old('hour', $online->hour) }}">
+                    @error('hour')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="mb-3">
                     <label for="desc" class="form-label">Tavsif</label>
